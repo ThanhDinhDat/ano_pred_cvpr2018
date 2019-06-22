aca204e99c752f4a445439bb940976f780c2fbf1
Mono Lu <luvinh.thinh@accellion.com>
2019-06-22 20:44:15 +0700

    add srgan generator

--- Codes/models.py
+++ Codes/models.py
@@ -13,13 +13,61 @@ slim = tf.contrib.slim
 
 
 def generator(inputs, layers, features_root=64, filter_size=3, pool_size=2, output_channel=3):
-    return unet.unet(inputs, layers, features_root, filter_size, pool_size, output_channel)
+    # return unet.unet(inputs, layers, features_root, filter_size, pool_size, output_channel)
+    return srgan_generator(inputs, layers, features_root, filter_size, pool_size, output_channel)
 
 
 def discriminator(inputs, num_filers=(128, 256, 512, 512)):
     logits, end_points = pix2pix.pix2pix_discriminator(inputs, num_filers)
     return logits, end_points['predictions']
 
+def srgan_generator(inputs, layers, features_root=64, filter_size=3, pool_size=2, output_channel=3):
+    # The Bx residual blocks
+    def residual_block(inputs, output_channel, stride, scope):
+        with tf.variable_scope(scope):
+            net = conv2(inputs, 3, output_channel, stride, use_bias=False, scope='conv_1')
+            net = batchnorm(net, True)
+            net = prelu_tf(net)
+            net = conv2(net, 3, output_channel, stride, use_bias=False, scope='conv_2')
+            net = batchnorm(net, True)
+            net = net + inputs
+
+        return net
+
+
+    with tf.variable_scope('generator_unit', reuse=reuse):
+        # The input layer
+        with tf.variable_scope('input_stage'):
+            net = conv2(inputs, 9, 64, 1, scope='conv')
+            net = prelu_tf(net)
+
+        stage1_output = net
+
+        # The residual block parts
+        for i in range(1, FLAGS.num_resblock+1 , 1):
+            name_scope = 'resblock_%d'%(i)
+            net = residual_block(net, 64, 1, name_scope)
+
+        with tf.variable_scope('resblock_output'):
+            net = conv2(net, 3, 64, 1, use_bias=False, scope='conv')
+            net = batchnorm(net, True)
+
+        net = net + stage1_output
+
+        with tf.variable_scope('subpixelconv_stage1'):
+            net = conv2(net, 3, 256, 1, scope='conv')
+            net = pixelShuffler(net, scale=2)
+            net = prelu_tf(net)
+
+        with tf.variable_scope('subpixelconv_stage2'):
+            net = conv2(net, 3, 256, 1, scope='conv')
+            net = pixelShuffler(net, scale=2)
+            net = prelu_tf(net)
+
+        with tf.variable_scope('output_stage'):
+            net = conv2(net, 9, output_channel, 1, scope='conv')
+
+    return net
 
 def flownet(input_a, input_b, height, width, reuse=None):
     net = FlowNetSD(mode=Mode.TEST)
