71baf27ebe18828fe47d1bae9483a634b9d2a8a0
Mono Lu <luvinh.thinh@accellion.com>
2019-06-22 21:07:29 +0700

    update missing funcs

--- Codes/models.py
+++ Codes/models.py
@@ -146,3 +146,60 @@ def initialize_flownet(sess, checkpoint):
     flownet_saver = tf.train.Saver(flownet_vars)
     print('FlownetSD restore from {}!'.format(checkpoint))
     flownet_saver.restore(sess, checkpoint)
+
+# Define the convolution building block
+def conv2(batch_input, kernel=3, output_channel=64, stride=1, use_bias=True, scope='conv'):
+    # kernel: An integer specifying the width and height of the 2D convolution window
+    with tf.variable_scope(scope):
+        if use_bias:
+            return slim.conv2d(batch_input, output_channel, [kernel, kernel], stride, 'SAME', data_format='NHWC',
+                            activation_fn=None, weights_initializer=tf.contrib.layers.xavier_initializer())
+        else:
+            return slim.conv2d(batch_input, output_channel, [kernel, kernel], stride, 'SAME', data_format='NHWC',
+                            activation_fn=None, weights_initializer=tf.contrib.layers.xavier_initializer(),
+                            biases_initializer=None)
+
+# Define our tensorflow version PRelu
+def prelu_tf(inputs, name='Prelu'):
+    with tf.variable_scope(name):
+        alphas = tf.get_variable('alpha', inputs.get_shape()[-1], initializer=tf.zeros_initializer(), dtype=tf.float32)
+    pos = tf.nn.relu(inputs)
+    neg = alphas * (inputs - abs(inputs)) * 0.5
+
+    return pos + neg
+
+# Define our Lrelu
+def lrelu(inputs, alpha):
+    return keras.layers.LeakyReLU(alpha=alpha).call(inputs)
+
+def batchnorm(inputs, is_training):
+    return slim.batch_norm(inputs, decay=0.9, epsilon=0.001, updates_collections=tf.GraphKeys.UPDATE_OPS,
+                        scale=False, fused=True, is_training=is_training)
+
+def phaseShift(inputs, scale, shape_1, shape_2):
+    # Tackle the condition when the batch is None
+    X = tf.reshape(inputs, shape_1)
+    X = tf.transpose(X, [0, 1, 3, 2, 4])
+
+    return tf.reshape(X, shape_2)
+
+# The implementation of PixelShuffler
+def pixelShuffler(inputs, scale=2):
+    size = tf.shape(inputs)
+    batch_size = size[0]
+    h = size[1]
+    w = size[2]
+    c = inputs.get_shape().as_list()[-1]
+
+    # Get the target channel size
+    channel_target = c // (scale * scale)
+    channel_factor = c // channel_target
+
+    shape_1 = [batch_size, h, w, channel_factor // scale, channel_factor // scale]
+    shape_2 = [batch_size, h * scale, w * scale, 1]
+
+    # Reshape and transpose for periodic shuffling for each channel
+    input_split = tf.split(inputs, channel_target, axis=3)
+    output = tf.concat([phaseShift(x, scale, shape_1, shape_2) for x in input_split], axis=3)
+
+    return output
